cfba68e63028971ae645433926477c7a2865e30e
Anes Benmerzoug <anes.benmerzoug@gmail.com>
2026-01-04 22:54:59 +0100

    Update script

--- scripts/run_pan_tilt_combinations.sh
+++ scripts/run_pan_tilt_combinations.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
-
 HOST="raspberrypi"
 EXECUTABLE="~/Projects/main"
-RERUN_DATA_FILE="pan_tilt.rrd"
-SERVO_MOTOR_DELAYS=("50" "100")
-ANGLE_STEPS=("5.0" "10.0")
+RERUN_DATA_FILE="data/pan_tilt.rrd"
+N_REPEAT=3
+SERVO_MOTOR_DELAYS=("500" "200" "100" "50" "10")
+ANGLE_STEPS=("1" "5" "15.0")
+RERUN_SERVER_IP=${RERUN_SERVER_IP:-"192.168.178.21"}
 
 # Start rerun server
 echo "Starting rerun server and streaming received data to file '$RERUN_DATA_FILE'"
@@ -22,12 +22,14 @@ trap cleanup_rerun_server EXIT;
 # Wait for rerun server to start
 sleep 1;
 
-for SERVO_MOTOR_DELAY in "${SERVO_MOTOR_DELAYS[@]}"
-do
-    for ANGLE_STEP in "${ANGLE_STEPS[@]}"
-    do
-        echo "Running executable '$EXECUTABLE' on host '$HOST' with servo motor delay $SERVO_MOTOR_DELAY ms and angle step $ANGLE_STEP deg";
-        ssh $HOST "$EXECUTABLE --servo-motor-delay=$SERVO_MOTOR_DELAY --angle-step=$ANGLE_STEP";
+for SERVO_MOTOR_DELAY in "${SERVO_MOTOR_DELAYS[@]}"; do
+    for ANGLE_STEP in "${ANGLE_STEPS[@]}"; do
+        echo "=================="
+        for i in $(seq $N_REPEAT); do
+            echo "Iteration $i: Running executable '$EXECUTABLE' on host '$HOST' with servo motor delay $SERVO_MOTOR_DELAY ms and angle step $ANGLE_STEP deg";
+            ssh $HOST "$EXECUTABLE --servo-motor-delay=$SERVO_MOTOR_DELAY --angle-step=$ANGLE_STEP --min-angle-top=0.0";
+            sleep 1;
+        done
     done
 done
 
